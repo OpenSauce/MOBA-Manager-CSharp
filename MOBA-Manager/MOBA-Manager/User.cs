0d7548a0f92336a5202c07f36313c45c1a24192b
Lawley <Lawley@LAWLEY-PC>
2018-09-01 23:30:34 +0100

    Created user and team class

---
+++ MOBA-Manager/MOBA-Manager/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOBA_Manager
+{
+
+    class User
+    {
+        private bool created = false;
+        private String name;
+        private int age;
+
+        public User(String name, int age, bool created)
+        {
+            this.name = name;
+            this.age = age;
+            this.created = created;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+    }
+}
