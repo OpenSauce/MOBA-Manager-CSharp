cd6934b61272ed589f788515b32a36d8b3d02e72
OpenSauce <lawleycobhc@gmail.com>
2019-06-25 21:21:23 +0100

    Complete Refactoring Required

--- MOBA-Manager/MOBA-Manager/MainWindow.xaml.cs
+++ MOBA-Manager/MOBA-Manager/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MOBA_Manager.src.Settings;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,46 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Xceed.Wpf.Toolkit;
 
 namespace MOBA_Manager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for IntoScreen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class IntoScreen : Window
     {
-        User _player;
-        PlaySession _session;
-        private Settings _gameSettings;
-
-        public MainWindow()
+        public IntoScreen()
         {
             InitializeComponent();
-            this._gameSettings = new Settings().LoadSettings();
-            this._session = new PlaySession();
-            MainFrame.Content = new UserCreation(this);
-        }
-
-        public void CreateTeam(User player)
-        {
-            this._player = player;
-            if (_player.IsCreated())
-            {
-                MainFrame.Content = new TeamCreation(this, _player, _session);
-            }
-        }
-
-        public void BeginGame()
-        {
-            MainFrame.Content = new MainGame(this, _player, _session);
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            SettingsPage settings = new SettingsPage(_gameSettings);
         }
     }
 }
