49bd95a180dd12b71bebe730a2c74136f9302d19
OpenSauce <lawleycobhc@gmail.com>
2021-02-24 22:56:58 +0000

    Last one

--- MOBA-Manager/MOBA-Manager/Source/Utilities/FixtureBuilder.cs
+++ MOBA-Manager/MOBA-Manager/Source/Utilities/FixtureBuilder.cs
@@ -1,6 +1,18 @@
-﻿namespace MOBA_Manager.Source.Utilities
+﻿using MOBA_Manager.DataModel;
+using MOBA_Manager.Source.DataModel;
+using System;
+using System.Collections.Generic;
+
+namespace MOBA_Manager.Source.Utilities
 {
-    internal class FixtureBuilder
+    public class FixtureBuilder
     {
+        private Team playerTeam;
+
+        public List<Fixture> GetNewFixtures(DateTime startingDate)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
